7715223f673c5e42b43871853a3d750322d67f80
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-06-10 09:51:29 +0530

    Create Reverse_ a_ Linked_ List_ in_ groups _of _given_ size.cpp

---
+++ Reverse_ a_ Linked_ List_ in_ groups _of _given_ size.cpp
@@ -0,0 +1,21 @@
+struct node *reverse (struct node *head, int k)
+{ 
+   struct node*Next=NULL;
+   struct node*prev=NULL;
+   struct node*curr=head;
+   int count=0;
+   while(curr!=NULL && count<k)
+   {
+       Next=curr->next;
+       curr->next=prev;
+       prev=curr;
+       curr=Next;
+       count++;
+   }
+   if(Next!=NULL)
+   {
+       head->next=reverse(Next,k);
+       
+   }
+   return prev;// Complete this method
+}
