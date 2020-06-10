8926e2ae3f2427e75ab78f4703826d2363b52102
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-06-10 09:48:08 +0530

    Create Rotate_a _Linked_ List.cpp

---
+++ Rotate_a _Linked_ List.cpp
@@ -0,0 +1,17 @@
+Node* rotate(Node* head, int k) {
+    Node*temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+    Node*end;
+    while(k--)
+    {
+        end=head;
+        head=head->next;
+    }
+    end->next=NULL;
+    
+    return head;// Your code here
+}
