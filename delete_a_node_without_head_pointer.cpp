5cae68b5236222b1bd2f361852b901c6fcc4fd52
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-06-10 10:06:03 +0530

    Create delete_a_node_without_head_pointer.cpp

---
+++ delete_a_node_without_head_pointer.cpp
@@ -0,0 +1,24 @@
+//method 1
+void deleteNode(Node *node)
+{
+    *node=*(node->next);// Your code here
+}
+//method 2
+void deleteNode(Node *node)
+{
+	if(!node->next)
+		return;
+		// if this the last node of list, it cannot be deleted
+	
+	Node* del = node->next;
+	// next node will be freed
+	
+	node->data = del->data;
+	// copying data of next node
+	
+	node->next = del->next;
+	// bypassing the next node
+	
+	free(del);
+	// freeing memory
+}
