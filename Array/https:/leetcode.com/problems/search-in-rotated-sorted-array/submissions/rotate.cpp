1d7570baa98c12d4173a30bdb2f43e69046fb0bd
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-07-26 09:52:14 +0530

    Create rotate.cpp

---
+++ Array/https:/leetcode.com/problems/search-in-rotated-sorted-array/submissions/rotate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        vector<int>::iterator it=find(nums.begin(),nums.end(),target);
+        if(it!=nums.end())
+        {
+            int index=distance(nums.begin(),it);
+            return index;
+        }
+        else
+        {
+            return -1;
+        }
+        
+    }
+};
