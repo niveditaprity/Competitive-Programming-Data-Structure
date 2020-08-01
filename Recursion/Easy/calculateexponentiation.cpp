4585b8847910f7c9f7a6e7f2dbc1ee1e0c46c4b1
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-08-01 11:41:11 +0530

    Create calculateexponentiation.cpp

---
+++ Recursion/Easy/calculateexponentiation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int power(int x,int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    return x*power(x,n-1);
+}
+
+int main() {
+	cout<<power(2,5)<<endl;
+	return 0;
+}
