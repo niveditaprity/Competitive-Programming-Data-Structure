4afae01715dfe6f21af4f1a2ef7552704a683f07
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-08-01 10:08:24 +0530

    Create factorial.cpp

---
+++ Recursion/Easy/factorial.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+int main() {
+	cout<<fact(5)<<endl;
+	return 0;
+}
